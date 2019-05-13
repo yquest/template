2890e5c12edd11951bfa31bec06615adbb97d833
fabm <francisco.ab.monteiro@gmail.com>
2019-05-13 15:25:21 +0100

    add validation to login

--- frontend/src/components/app/LoginEditor.tsx
+++ frontend/src/components/app/LoginEditor.tsx
@@ -1,10 +1,11 @@
 import { observer } from "mobx-react";
 import * as React from "react";
-import { action, observable } from "mobx";
+import { action, observable, computed } from "mobx";
 import { userService } from "../../services/UserService";
 import { UserRegisterEditor } from "./UserRegisterEditor";
 import { User } from "../../model/User";
 import { notificationStore, NotificationType } from "./Notifications";
+import { AppInput, InputType } from "../general/appTextInput";
 
 export interface LoginProps {
   loginSuccessefull(user: string);
@@ -16,14 +17,49 @@ class LoginStore {
   username: string = "";
   @observable
   password: string = "";
+  @observable
+  startUsernameValidation:boolean = false;
+  @observable
+  startPasswordValidation:boolean = false;
+
+  @computed
+  get passwordError(): string {
+    if(!this.startPasswordValidation){
+      return null;
+    }
+    if(this.password.length === 0){
+      return "required";
+    }else{
+      return "";
+    }
+  }
+
+  @computed
+  get usernameError(): string {
+    if(!this.startUsernameValidation){
+      return null;
+    }
+    if(this.username.length === 0){
+      return "required";
+    }else{
+      return "";
+    }
+  }
+
+  @computed
+  get isAllValid() {
+    return this.passwordError === "" && this.usernameError === "";
+  }
 
   @action
   updateUserName(value: string) {
+    this.startUsernameValidation = true;
     this.username = value;
   }
 
   @action
   updatePassword(value: string) {
+    this.startPasswordValidation = true;
     this.password = value;
   }
 }
@@ -38,43 +74,26 @@ export class LoginEditor extends React.Component<LoginProps, {}> {
         <h3>Login</h3>
         <div
           className="card"
-          style={{ marginTop: "1rem", marginBottom: "1rem", padding: "1.5rem" }}
-        >
+          style={{
+            marginTop: "1rem",
+            marginBottom: "1rem",
+            padding: "1.5rem"
+          }}>
           <div className="col-12">
-            <div className="form-group row">
-              <label
-                htmlFor="example-datetime-local-input"
-                className="col-2 col-form-label"
-              >
-                Username
-              </label>
-            </div>
-            <div className="col-6">
-              <input
-                className="form-control"
-                type="text"
-                onChange={event =>
-                  loginStore.updateUserName(event.target.value)
-                }
-              />
-            </div>
-            <div className="form-group row">
-              <label
-                htmlFor="example-datetime-local-input"
-                className="col-2 col-form-label"
-              >
-                Password
-              </label>
-            </div>
-            <div className="col-6">
-              <input
-                className="form-control"
-                type="password"
-                onChange={event =>
-                  loginStore.updatePassword(event.target.value)
-                }
-              />
-            </div>
+            <AppInput
+              label="Username"
+              labelId="username"
+              inputType={InputType.TEXT}
+              onChange={value => loginStore.updateUserName(value)}
+              error={loginStore.usernameError}
+            />
+            <AppInput
+              label="Password"
+              labelId="password"
+              inputType={InputType.PASSWORD}
+              onChange={value => loginStore.updatePassword(value)}
+              error={loginStore.passwordError}
+            />
           </div>
         </div>
 
@@ -83,31 +102,32 @@ export class LoginEditor extends React.Component<LoginProps, {}> {
           type="button"
           className="btn btn-primary"
           onClick={() => {
-            let user:User = {
-              username : loginStore.username,
-              password : loginStore.password
+            if (!loginStore.isAllValid) {
+              return;
+            }
+            let user: User = {
+              username: loginStore.username,
+              password: loginStore.password
             };
-            userService.userLogin(user).then((res)=>{
-              if(res.status === 200){
+            userService.userLogin(user).then(res => {
+              if (res.status === 200) {
                 this.props.loginSuccessefull(user.username);
-              }else{
+              } else {
                 console.log("fails");
                 let notification = notificationStore.createNotification();
                 notification.content = "Authentication fails";
                 notification.type = NotificationType.ERROR;
-                notificationStore.addNotificationTemp(notification,3000);
+                notificationStore.addNotificationTemp(notification, 3000);
               }
             });
-          }}
-        >
+          }}>
           login
         </button>
         <button
           style={{ marginRight: "0.5rem" }}
           type="button"
           className="btn btn-primary"
-          onClick={() => this.props.showUserRegister()}
-        >
+          onClick={() => this.props.showUserRegister()}>
           sign in
         </button>
       </div>
