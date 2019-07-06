ec70247b97a9d2b78179cde54eef48d48337388f
fabm <francisco.ab.monteiro@gmail.com>
2019-07-06 13:31:00 +0100

    remove memory dao

--- web/memory-dao/src/main/kotlin/pt/fabm/template/dao/DaoMemoryShared.kt
+++
@@ -1,9 +0,0 @@
-package pt.fabm.template.dao
-
-import pt.fabm.template.models.Car
-import pt.fabm.template.models.UserRegisterIn
-
-object DaoMemoryShared {
-  lateinit var users:MutableMap<String,UserRegisterIn>
-  lateinit var cars:MutableList<Car>
-}
